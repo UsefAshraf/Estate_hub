1b8385c7697a6e082cda3fd92c1c5fa874281b32
Mai Mohamed <maielkhateb874@gmail.com>
2025-11-25 22:18:16 +0200

    Adding user management

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 // src/App.tsx
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer"; // import Footer
+//import Propertydetail from "./components/Propertydetail/propertydetail";
+import UserManagement from "./pages/admin/usermanagement";
+//import AppRoutes from "./routes/AppRoutes";
+//import Navbar from "./components/Navbar/Navbar";
+//import Footer from "./components/Footer/Footer"; // import Footer
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="mt-4">
-        <AppRoutes />
-      </div>
-      <Footer />
+      
+      <UserManagement></UserManagement>
     </Router>
   );
 };
